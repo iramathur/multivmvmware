b821b6bafc216148923b9e032f475d2fa89fb7d8
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-21 22:49:47 +0530

    Update multivmvmware.tf

--- multivmvmware.tf
+++ multivmvmware.tf
@@ -26,6 +26,11 @@ data "vsphere_network" "network" {
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
+data "vsphere_virtual_machine" "template" {
+  name          = "ubuntu16"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
   resource_pool_id     = "${data.vsphere_resource_pool.pool.id}"
@@ -34,7 +39,7 @@ resource "vsphere_virtual_machine" "vm" {
   folder               = "dev_zone"
   num_cpus = 2
   memory   = 1024
-  guest_id = "ubuntu16"
+  guest_id             = "${data.vsphere_virtual_machine.template.guest_id}"
 
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
