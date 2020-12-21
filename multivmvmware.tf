28d19f8b27b73b17cc1d16919d2dc6361baa3a9e
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-21 22:40:05 +0530

    Update multivmvmware.tf

--- multivmvmware.tf
+++ multivmvmware.tf
@@ -7,9 +7,13 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-#### RETRIEVE DATA INFORMATION ON VCENTER ####
 data "vsphere_datacenter" "dc" {
   name = "devcloud"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "vmstore"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_resource_pool" "pool" {
@@ -17,59 +21,27 @@ data "vsphere_resource_pool" "pool" {
  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-# Retrieve datastore information on vsphere
-data "vsphere_datastore" "datastore" {
-  name          = "vmstore"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-# Retrieve network information on vsphere
 data "vsphere_network" "network" {
   name          = "portGroup-1004"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-# Retrieve template information on vsphere
-data "vsphere_virtual_machine" "template" {
-  name          = "ubuntu16"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-# Set vm parameters
-resource "vsphere_virtual_machine" "vm-one" {
-  count = 2
-  name                 = "vm-multi-${count.index}"
-  num_cpus             = 2
-  memory               = 4096
-  datastore_id         = "${data.vsphere_datastore.datastore.id}"
-  #host_system_id       = "${data.vsphere_host.host.id}"
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
   resource_pool_id     = "${data.vsphere_resource_pool.pool.id}"
-  guest_id             = "${data.vsphere_virtual_machine.template.guest_id}"
-  scsi_type            = "${data.vsphere_virtual_machine.template.scsi_type}"
-  folder            = "dev_zone"
-  # Set network parameters
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  wait_for_guest_net_timeout = -1
+  folder = "dev_zone"
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "centos7_64Guest"
+
   network_interface {
-    network_id         = "${data.vsphere_network.network.id}"
+    network_id = "${data.vsphere_network.network.id}"
   }
-  # Use a predefined vmware template has main disk
+
   disk {
-    label = "vm-multi-${count.index}.vmdk"
-    size = "30"
-    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+    label = "disk0"
+    size  = 20
   }
-  clone {
-    template_uuid = "${data.vsphere_virtual_machine.template.id}"
-    customize {
-      linux_options {
-        host_name = "vm-multi-${count.index}"
-        domain    = "test.internal"
-      }
-      network_interface {
-      }
-      ipv4_gateway = "10.198.4.1"
-    }
-
-  }
-
 }
-
