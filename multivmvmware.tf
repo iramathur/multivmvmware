9b83ba59510f0a4564616a0c2b9d9089059daf5a
itom-qe <50704200+itom-qe@users.noreply.github.com>
2021-02-01 12:37:04 +0530

    Update multivmvmware.tf

--- multivmvmware.tf
+++ multivmvmware.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
-  user           = "cmpqa.svc@itomcmp.servicenow.com"
-  password       = "snc!23$"
-  vsphere_server = "10.198.1.13"
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "${var.host}"
   version = "< 1.16.0"
   # If you have a self-signed cert
   allow_unverified_ssl = true
